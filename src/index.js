b01bcbe384f32d87e4073c8c3f4f9955c40d6905
Erik Muir <erikdmuir@gmail.com>
2020-07-27 17:25:44 -0500

    implemented pagination; added dry-run flag

--- src/index.js
+++ src/index.js
@@ -1,49 +1,67 @@
 const fs = require("fs");
 const Dropbox = require("dropbox").Dropbox;
 const fetch = require("isomorphic-fetch");
-const accessToken =
-  "1Z7GgIhHDAgAAAAAAAAtrGz-KGtTF4ogAD-pdCYKSbokkiUIHSni3jYoGZXfpJdW";
+
+const accessToken = "1Z7GgIhHDAgAAAAAAAAtrGz-KGtTF4ogAD-pdCYKSbokkiUIHSni3jYoGZXfpJdW";
 const dropbox = new Dropbox({ fetch, accessToken });
 const args = process.argv.slice(2);
 const _destinationRoot = args[0] || "dropbox-migration";
+const dryRun = args.includes('--dry-run');
 
-async function listFolderContents(path = "") {
-  return await dropbox.filesListFolder({ path });
+async function getFolderContents(path = "") {
+  const contents = { files: [], folders: [] };
+
+  let response = await dropbox.filesListFolder({ path });
+  populateFolderContents(response, contents);
+
+  while (response.has_more) {
+    response = await dropbox.filesListFolderContinue({ cursor: response.cursor });
+    populateFolderContents(response, contents);
+  }
+  return contents;
 }
 
-function filterFolders(contents) {
-  return contents.entries
+function populateFolderContents(response, contents) {
+  contents.files = [...contents.files, ...filterFiles(response)];
+  contents.folders = [...contents.folders, ...filterFolders(response)];
+}
+
+function filterFolders(response) {
+  return response.entries
     .filter((x) => x[".tag"] === "folder")
     .map((x) => x.path_display);
 }
 
-function filterFiles(contents) {
-  return contents.entries
+function filterFiles(response) {
+  return response.entries
     .filter((x) => x[".tag"] === "file" && x.is_downloadable)
     .map((x) => x.path_display);
 }
 
-async function downloadFile(file) {
-  console.log(` - downloading file: ${file}`);
-  // curl -s -X POST https://content.dropboxapi.com/2/files/download \
-  //   --header "Authorization: Bearer $access_token" \
-  //   --header "Dropbox-API-Arg: {\"path\": \"$file\"}" \
-  //   --output "${file:1}" # strip leading slash
+async function downloadFile(path) {
+  console.log(` - downloading file: ${path}`);
+  if (dryRun) {
+    fs.writeFileSync(path.substring(1), "");
+  } else {
+    const response = await dropbox.filesDownload({ path });
+    if (response.fileBinary) {
+      fs.writeFileSync(path.substring(1), response.fileBinary, { encoding: 'binary' });
+    }
+  }
 }
 
 async function processFolder(path = '') {
   console.log(`Processing folder: ${path || "/"}`);
   fs.mkdirSync(`./${_destinationRoot}${path}`, { recursive: true });
-  const contents = await listFolderContents(path);
-  const files = filterFiles(contents);
-  const folders = filterFolders(contents);
-  await files.forEach(async (file) => await downloadFile(file));
-  await folders.forEach(async (folder) => await processFolder(folder));
+  const contents = await getFolderContents(path);
+  await contents.files.forEach(async (file) => await downloadFile(file));
+  await contents.folders.forEach(async (folder) => await processFolder(folder));
 }
 
 async function run() {
   fs.rmdirSync(_destinationRoot, { recursive: true });
   await processFolder();
+  console.log("DONE!");
 }
 
 run();
